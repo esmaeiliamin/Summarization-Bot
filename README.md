75f317e9791c4679b2c9e3f0d480027e1d07ef13
Amin <aminesmaeili439@gmail.com>
2026-02-25 14:06:44 +0330

    add new code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Summarization-Bot
+# This function helps to break down the summarization task into smaller 
+# parts using the map-reduce paradigm from LangChain,processes each
+# part with the LLm, and then combines the results into a final summary.
+# This implementation can handle large documents by splitting them into 
+# smaller chunks and processing them parallely.
--- src/chain.py
+++ src/chain.py
@@ -1,2 +1,21 @@
 import os
 import time
+from langchain.chains import MapReduceDocumentsChain, LLMChain, ReduceDocumentsChain, StuffDocumentsChain
+from langchain.document_loaders import NewsURLLoader
+from langchain.llms import CTransformers
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+def summarizer(article_url):
+    # Load article
+    loader = NewsURLLoader([article_url])
+    docs = loader.load()
+    # Load LLM
+    config = {'max_new_tokens': 4096, 'temperature': 0.3, 'context_length': 4096}
+    llm = CTransformers(model="TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
+                        model_file = "mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+                        config = config,
+                        threads = os.cpu_count())
+    # Map template and chain
+    
