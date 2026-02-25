a9c6b7704cabcfb540d531b91dbbd3e297df2822
Amin <aminesmaeili439@gmail.com>
2026-02-25 14:37:08 +0330

    add new code

--- src/chain.py
+++ src/chain.py
@@ -18,4 +18,45 @@ def summarizer(article_url):
                         config = config,
                         threads = os.cpu_count())
     # Map template and chain
+    map_template = """<s>[INST] The following is a part of an article from
+    a website :
+    {docs}
+    Based on this, please identify the main theme and key points of it
+    Answer: [/INST]</s>"""
+    map_prompt = PromptTemplate.form_template(map_template)
+    map_chain = LLMChain(llm=llm, prompt=map_prompt)
+    # Reduce template and chain
+    reduce_template = """<s>[INST] The following is set of summaries from
+    the article:
+    {doc_summaries}
+    Take these as a input and condense it into a final, consolidated summary
+    of all the main points.
+    Construct it as a well organized summary of the main points and it hould
+    be between 2 and 6 paragraphs.
+    Answer: [/INST]</s>"""
+    reduce_prompt = PromptTemplate.from_template(reduce_template)
+    reduce_chain = LLMChain(llm,prompt=reduce_prompt)
+    # Take a list of documents, combines them into a single string, and passes this to an LLMChain
+    combine_documents_chain = StuffDocumentsChain(llm_chain=reduce_chain, document_variable_name="doc_summaries")
+    # Combines and iteratively reduces the mapped documents
+    reduce_documents_chain = ReduceDocumentsChain(
+        # This is final chain that is called.
+        combine_documents_chain=combine_documents_chain,
+        # If documents exceed context for 'StuffDocumentsChain'
+        collapse_documents_chain=combine_documents_chain,
+        # The maximum number of tokens to group documents into
+        tokens_max=4000,
+    )
+    # Combinen documents by mapping a chain over them, then combining results
+    map_reduce_chain = MapReduceDocumentsChain(
+        # Map chain
+        llm_chain=map_chain,
+        # Reduce chain
+        reduce_documents_chain=reduce_documents_chain,
+        # The variable name in the llm_chain to put the documents in 
+        document_variable_name="docs",
+        # Return the results of the map steps in the output
+        return_intermediate_steps=True,
+    )
     
+
